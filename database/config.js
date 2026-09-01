0466d163937a3097ec04ff305a9ad86ad3014f78
dipzy086 <warningzone5555@gmail.com>
2026-09-01 14:06:06 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tokens: "8600140277:AAEgIgJjV76-oit1ciAOw69KAQGFaaCwzT8",  // Masukin Bot token kamu
+  owners: "7552335798", // Masukin ID Telegram kamu
+  port: "2042", // Masukin Port panel kamu 
+  ipvps: "http://celloprivate.vortexx.web.id/" // Masukin IP vps kamu atau domain panel kamu yg asalnya ( https://AiiSigma.id ) menjadi ( http://AiiSigma.id )
+};
+
+/* WAJIB BACA !!!
+ cari di index.js halaman 1710-1711 di situ masukin bot token sama id tele lu.
+
+ emngnya knpa bang?
+ lu buka aja dulu. di sana udah gw kasi penjelasannya
+*/
